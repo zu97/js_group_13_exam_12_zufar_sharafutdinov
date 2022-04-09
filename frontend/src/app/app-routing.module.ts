fc1f91a2283040f8ec8cf6c8a88d550aa506fcdc
admin <admin@admin.com>
2022-04-09 15:27:54 +0600

    Updating the layout, displaying information about the logged in user for the frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- frontend/src/app/ui/layout/layout.component.ts
+++ frontend/src/app/ui/layout/layout.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/types';
+import { logoutUserRequest } from '../../store/users.actions';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent { }
+export class LayoutComponent {
+  user: Observable<null | User>;
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.user = store.select(state => state.users.user);
+  }
+
+  logout() {
+    this.store.dispatch(logoutUserRequest());
+  }
+}
