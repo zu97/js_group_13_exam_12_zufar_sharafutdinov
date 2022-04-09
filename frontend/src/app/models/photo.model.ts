d63dc6cd0ec52b21622d800fe9e4c899e4999cc8
admin <admin@admin.com>
2022-04-09 18:25:20 +0600

    Creating a model for working with photos for the frontend

---
+++ frontend/src/app/models/photo.model.ts
@@ -0,0 +1,26 @@
+export interface Photo {
+  _id: string;
+  user: {
+    _id: string;
+    displayName: string;
+  };
+  title: string;
+  image: string;
+}
+
+export interface AddPhotoData {
+  [key: string]: any;
+  title: string;
+  image: string;
+}
+
+export interface FieldError {
+  message: string;
+}
+
+export interface AddError {
+  errors: {
+    title?: undefined | FieldError;
+    image?: undefined | FieldError;
+  }
+}
