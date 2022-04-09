19134c9cabfab5650af4d5b1d22fd3ff8eb20e79
admin <admin@admin.com>
2022-04-09 12:38:20 +0600

    Creating routing and 404 page for the frontend

---
+++ frontend/src/app/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-not-found',
+  template: `<h3  class="not-found">Page not found!</h3>`,
+  styles: ['.not-found { text-align: center; padding: 16px 0; font-weight: bold; }']
+})
+export class PageNotFoundComponent {}
